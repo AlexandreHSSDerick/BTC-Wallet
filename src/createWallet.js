22ce768912ea3c5be2dc870dd7a6f29fc9bfc244
AlexandreHSSDerick <alexandreorderick@gmail.com>
2024-07-24 22:56:37 -0300

    HDwalletDerivation

--- src/createWallet.js
+++ src/createWallet.js
@@ -4,5 +4,12 @@ const bip39 = require('bip39')
 const bitcoin = require('bitcoinjs-lib')
 
 //Set network
+//bitcoin - principal network - mainnet | test network - testnet
 const network = bitcoin.networks.testnet
+
+//HD wallets derivation
+//if `m/49'/1'/0'/0` - testnet
+//if `m/49'/0'/0'/0`
+const path = `m/49'/1'/0'/0`
+
 
