010e872610a63982f722f4d5e533cd31b5ac38ed
AlexandreHSSDerick <alexandreorderick@gmail.com>
2024-07-24 22:47:19 -0300

    DependeciesImport

---
+++ src/createWallet.js
@@ -0,0 +1,8 @@
+//Dependecies Import
+const bip32 = require('bip32')
+const bip39 = require('bip39')
+const bitcoin = require('bitcoinjs-lib')
+
+//Set network
+const network = bitcoin.networks.testnet
+
