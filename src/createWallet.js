9c15a9fa1d8564da54ffc1746f1b2998dee5d244
AlexandreHSSDerick <alexandreorderick@gmail.com>
2024-07-24 23:08:21 -0300

    WalletRoot

--- src/createWallet.js
+++ src/createWallet.js
@@ -1,13 +1,13 @@
-//Dependecies Import
+//dependecies Import
 const bip32 = require('bip32')
 const bip39 = require('bip39')
 const bitcoin = require('bitcoinjs-lib')
 
-//Set network
+//set network
 //bitcoin - principal network - mainnet | test network - testnet
 const network = bitcoin.networks.testnet
 
-//HD wallets derivation
+//HD wallet derivation
 //if `m/49'/1'/0'/0` - testnet
 //if `m/49'/0'/0'/0`
 const path = `m/49'/1'/0'/0`
@@ -15,5 +15,8 @@ const path = `m/49'/1'/0'/0`
 //mnemonic is the generator of the safe words in the seed
 let mnemonic = bip39.generateMnemonic()
 const seed = bip39.mnemonicToSeedSync(mnemonic)
+
+//deterministic wallet root HD
+let root = bip32.fromSeed(seed, network)
 
 
